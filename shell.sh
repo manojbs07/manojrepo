f49d8047ba0181024e9d93b6be0924742a1052ac
manoj <manojsirigere@gmail.com>
2022-11-27 18:04:25 +0000

    shell script

---
+++ shell.sh
@@ -0,0 +1,18 @@
+#Shell script 
+
+#!/bin/bash
+
+#shell script to display given number pyramid pattern
+
+row=5
+
+for((i=1;i<=row;i++))
+do
+   for((j=1;j<=i;j++))
+   do
+   echo -n "$j "
+done
+echo
+done
+
+
